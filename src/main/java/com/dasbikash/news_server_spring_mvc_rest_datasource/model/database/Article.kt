b329fc4e20d4b9180499572abe61ab205391cb36
Bikash Das <bikashdaseee@gmail.com>
2019-04-09 10:59:34 +0600

    Init commit. Fixes dasBikash84/NewsServer_reloaded_master#6

---
+++ src/main/java/com/dasbikash/news_server_spring_mvc_rest_datasource/model/database/Article.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 das.bikash.dev@gmail.com. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dasbikash.news_server_spring_mvc_rest_datasource.model.database
+
+import com.dasbikash.news_server_parser.model.ArticleImage
+import java.util.*
+import javax.persistence.*
+import kotlin.collections.ArrayList
+
+
+@Entity
+@Table(name = DatabaseTableNames.ARTICLE_TABLE_NAME)
+data class Article(
+        @Id
+        var id: String="",
+
+        @ManyToOne(targetEntity = Page::class, fetch = FetchType.EAGER)
+        @JoinColumn(name = "pageId")
+        var page: Page? = null,
+
+        var title: String? = null,
+        var modificationTS: Date? = null,
+        var publicationTS: Date? = null,
+
+        @Column(columnDefinition = "text")
+        var articleText: String? = null,
+
+        @ElementCollection(targetClass = ArticleImage::class)
+        @CollectionTable(name = "image_links", joinColumns = [JoinColumn(name = "articleId")])
+        @Column(name = "imageLink", columnDefinition = "text")
+        var imageLinkList: List<ArticleImage> = ArrayList(),
+
+        @Column(columnDefinition = "text")
+        var previewImageLink: String? = null
+){
+        override fun toString(): String {
+                return "Article(id='$id', page=${page?.name}, title=$title, modificationTS=$modificationTS, publicationTS=$publicationTS, " +
+                        "articleText=$articleText, imageLinkList=$imageLinkList, previewImageLink=$previewImageLink)"
+        }
+}
---
+++ src/main/java/com/dasbikash/news_server_spring_mvc_rest_datasource/model/database/Page.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 das.bikash.dev@gmail.com. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dasbikash.news_server_spring_mvc_rest_datasource.model.database
+
+import javax.persistence.*
+
+@Entity
+@Table(name = DatabaseTableNames.PAGE_TABLE_NAME)
+data class Page(
+        @Id
+        var id: String="",
+
+        @ManyToOne(targetEntity = Newspaper::class,fetch = FetchType.EAGER)
+        @JoinColumn(name="newsPaperId")
+        var newspaper: Newspaper?=null,
+
+        var parentPageId: String?=null,
+        var name: String?=null,
+
+        var active: Boolean = true,
+
+        @OneToMany(fetch = FetchType.LAZY,mappedBy = "page",targetEntity = Article::class)
+        var articleList: List<Article>?=null
+
+){
+    companion object {
+        @JvmField
+        val TOP_LEVEL_PAGE_PARENT_ID = "PAGE_ID_0"
+        @JvmField
+        val DEFAULT_LINK_TRAILING_FORMAT = "page_num"
+    }
+
+    @Transient
+    fun isTopLevelPage():Boolean{
+        return parentPageId == TOP_LEVEL_PAGE_PARENT_ID
+    }
+
+    override fun toString(): String {
+        return "Page(id='$id', newspaper=${newspaper?.name}, parentPageId=$parentPageId, name=$name, active=$active)"
+    }
+
+
+}
